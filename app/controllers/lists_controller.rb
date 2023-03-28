a9f760a0081880d450e3646823ce52f7b456e19a
natsumikobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2023-03-28 14:08:50 +0000

    [Add]rubocop_test

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,9 +2,9 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
-     @list = List.new(list_params)
+    @list = List.new(list_params)
     if @list.save
       flash[:notice] = "投稿が成功しました"
       redirect_to list_path(@list.id)
@@ -24,21 +24,21 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
-    redirect_to '/lists'
+    redirect_to "/lists"
   end
 
   private
-  def list_params
-    params.require(:list).permit(:title, :body, :image)
-  end
+    def list_params
+      params.require(:list).permit(:title, :body, :image)
+    end
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe List, "モデルに関するテスト", type: :model do
-  describe '実際に保存してみる' do
+  describe "実際に保存してみる" do
     it "有効な投稿内容の場合は保存されるか" do
       expect(FactoryBot.build(:list)).to be_vaild
     end
